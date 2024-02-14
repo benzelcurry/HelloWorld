a637fff9e81eca9bb72ba9502a786d855cdc3d97
Benzel <benawhiting@gmail.com>
2024-02-13 16:35:50 -0800

    Create an instance of GoldenRetriever

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,44 +1,56 @@
 ﻿using HelloWorld;
-using System.Diagnostics;
+
+GoldenRetriever goldenRetriever = new();
+
+Console.WriteLine
+(
+    $"A {goldenRetriever.Breed()} has {goldenRetriever.NumberOfLegs()} legs," +
+    $"has " + (goldenRetriever.HasTail() ? "a" : "no") + $" tail, and {goldenRetriever.NumberOfEyes()} eyes."
+);
+
+Console.WriteLine($"A {goldenRetriever.Breed()} says: ");
+goldenRetriever.Sound();
+
+//using System.Diagnostics;
 
 //NameClass nameClass = new NameClass();
-AgeClass ageClass = new AgeClass();
+//AgeClass ageClass = new AgeClass();
 
-TextWriterTraceListener fileListener = new TextWriterTraceListener("log.txt");
-Trace.Listeners.Add(fileListener);
-Trace.Listeners.Add(new ConsoleTraceListener());
+//TextWriterTraceListener fileListener = new TextWriterTraceListener("log.txt");
+//Trace.Listeners.Add(fileListener);
+//Trace.Listeners.Add(new ConsoleTraceListener());
 
-Trace.WriteLine("Application started");
+//Trace.WriteLine("Application started");
 
-try
-{
-    Trace.WriteLine("Trying to calculate the ages.");
+//try
+//{
+//    Trace.WriteLine("Trying to calculate the ages.");
 
-    Trace.WriteLine("Calculating year 1996");
-    int currentAge = ageClass.CalculateAge(1996);
+//    Trace.WriteLine("Calculating year 1996");
+//    int currentAge = ageClass.CalculateAge(1996);
 
-    Trace.WriteLine("Calculating year 1899");
-    int currentAge2 = ageClass.CalculateAge(1899);
-    Console.WriteLine($"You are {currentAge} years old");
-}
-catch (Exception ex)
-{
-    if (ex.Message.Contains("Year can't be 0 or less"))
-    {
-        Console.WriteLine("Please enter a year that is higher than 0.");
-    }
-    else if (ex.Message.Contains("You can't be born in the future!"))
-    {
-        Console.WriteLine("You can't be born in the future.");
-    }
-    else
-    {
-        Console.WriteLine($"Something went wrong: {ex.Message}");
-    }    
+//    Trace.WriteLine("Calculating year 1899");
+//    int currentAge2 = ageClass.CalculateAge(1899);
+//    Console.WriteLine($"You are {currentAge} years old");
+//}
+//catch (Exception ex)
+//{
+//    if (ex.Message.Contains("Year can't be 0 or less"))
+//    {
+//        Console.WriteLine("Please enter a year that is higher than 0.");
+//    }
+//    else if (ex.Message.Contains("You can't be born in the future!"))
+//    {
+//        Console.WriteLine("You can't be born in the future.");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"Something went wrong: {ex.Message}");
+//    }    
     
-}
+//}
 
-fileListener.Close();
+//fileListener.Close();
 
 
 //while (string.IsNullOrEmpty(nameClass.Name))
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f78b7958e6016b30a5ad3ba012390ba6eaa0df9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3068dd214fe66bd24cb26c02aec3ea39495e7f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
