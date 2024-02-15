beb9ab68bbd0ca186c9a0be615fa6fef51221692
Benzel <benawhiting@gmail.com>
2024-02-14 16:06:51 -0800

    Add business layer to solution

---
+++ HelloWorld.Business/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWorld.Business
+{
+    public class Class1
+    {
+
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,57 +1,44 @@
 ﻿using HelloWorld;
-
-GoldenRetriever goldenRetriever = new();
-goldenRetriever.Legs = 4;
-
-Console.WriteLine
-(
-    $"A {goldenRetriever.Breed()} has {goldenRetriever.Legs} legs," +
-    $" has " + (goldenRetriever.HasTail() ? "a" : "no") + $" tail, and {goldenRetriever.NumberOfEyes()} eyes."
-);
-
-Console.WriteLine($"A {goldenRetriever.Breed()} says: ");
-goldenRetriever.Sound();
-
-//using System.Diagnostics;
+using System.Diagnostics;
 
 //NameClass nameClass = new NameClass();
-//AgeClass ageClass = new AgeClass();
+AgeClass ageClass = new AgeClass();
 
-//TextWriterTraceListener fileListener = new TextWriterTraceListener("log.txt");
-//Trace.Listeners.Add(fileListener);
-//Trace.Listeners.Add(new ConsoleTraceListener());
+TextWriterTraceListener fileListener = new TextWriterTraceListener("log.txt");
+Trace.Listeners.Add(fileListener);
+Trace.Listeners.Add(new ConsoleTraceListener());
 
-//Trace.WriteLine("Application started");
+Trace.WriteLine("Application started");
 
-//try
-//{
-//    Trace.WriteLine("Trying to calculate the ages.");
+try
+{
+    Trace.WriteLine("Trying to calculate the ages.");
 
-//    Trace.WriteLine("Calculating year 1996");
-//    int currentAge = ageClass.CalculateAge(1996);
+    Trace.WriteLine("Calculating year 1996");
+    int currentAge = ageClass.CalculateAge(1996);
 
-//    Trace.WriteLine("Calculating year 1899");
-//    int currentAge2 = ageClass.CalculateAge(1899);
-//    Console.WriteLine($"You are {currentAge} years old");
-//}
-//catch (Exception ex)
-//{
-//    if (ex.Message.Contains("Year can't be 0 or less"))
-//    {
-//        Console.WriteLine("Please enter a year that is higher than 0.");
-//    }
-//    else if (ex.Message.Contains("You can't be born in the future!"))
-//    {
-//        Console.WriteLine("You can't be born in the future.");
-//    }
-//    else
-//    {
-//        Console.WriteLine($"Something went wrong: {ex.Message}");
-//    }    
-    
-//}
+    Trace.WriteLine("Calculating year 1899");
+    int currentAge2 = ageClass.CalculateAge(1899);
+    Console.WriteLine($"You are {currentAge} years old");
+}
+catch (Exception ex)
+{
+    if (ex.Message.Contains("Year can't be 0 or less"))
+    {
+        Console.WriteLine("Please enter a year that is higher than 0.");
+    }
+    else if (ex.Message.Contains("You can't be born in the future!"))
+    {
+        Console.WriteLine("You can't be born in the future.");
+    }
+    else
+    {
+        Console.WriteLine($"Something went wrong: {ex.Message}");
+    }
 
-//fileListener.Close();
+}
+
+fileListener.Close();
 
 
 //while (string.IsNullOrEmpty(nameClass.Name))
@@ -62,3 +49,15 @@ goldenRetriever.Sound();
 //nameClass.GetLastName();
 
 //nameClass.ShowName();
+
+//GoldenRetriever goldenRetriever = new();
+//goldenRetriever.Legs = 4;
+
+//Console.WriteLine
+//(
+//    $"A {goldenRetriever.Breed()} has {goldenRetriever.Legs} legs," +
+//    $" has " + (goldenRetriever.HasTail() ? "a" : "no") + $" tail, and {goldenRetriever.NumberOfEyes()} eyes."
+//);
+
+//Console.WriteLine($"A {goldenRetriever.Breed()} says: ");
+//goldenRetriever.Sound();
