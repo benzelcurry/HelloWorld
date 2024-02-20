a109c902d8db07c90e1155c3e978e8449916211c
Benzel <benawhiting@gmail.com>
2024-02-20 14:46:51 -0800

    Add labels, button, and text field

--- HelloWorld.WinForms/Form1.Designer.cs
+++ HelloWorld.WinForms/Form1.Designer.cs
@@ -28,18 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
             button1 = new Button();
             SuspendLayout();
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(82, 92);
+            label1.Name = "label1";
+            label1.Size = new Size(167, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Year of birth:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(82, 319);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Result:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(285, 319);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 37);
+            label3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(285, 89);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(691, 43);
+            textBox1.TabIndex = 3;
+            // 
             // button1
             // 
-            button1.Location = new Point(188, 148);
+            button1.Location = new Point(285, 211);
             button1.Name = "button1";
-            button1.Size = new Size(169, 52);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Size = new Size(203, 52);
+            button1.TabIndex = 4;
+            button1.Text = "Calculate Age";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // Form1
             // 
@@ -47,13 +83,22 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1172, 705);
             Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
         private Button button1;
     }
 }
