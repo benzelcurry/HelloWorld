6f0bc8fba5e28d22a8a631d129708ae0214f79fe
Benzel <benawhiting@gmail.com>
2024-02-20 14:28:39 -0800

    Initialize a WinForm

--- HelloWorld.Business/obj/Debug/net8.0/HelloWorld.Business.AssemblyInfo.cs
+++ HelloWorld.Business/obj/Debug/net8.0/HelloWorld.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb9ab68bbd0ca186c9a0be615fa6fef51221692")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ac14b17f5e434a6663555223080481bd43a9a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
