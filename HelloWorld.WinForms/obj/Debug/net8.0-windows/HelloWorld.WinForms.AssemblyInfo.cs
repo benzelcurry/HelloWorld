672607b9ce97d82b74eaa06b94e782c1a3a54e63
Benzel <benawhiting@gmail.com>
2024-02-23 14:05:37 -0800

    Create a new MDI Form

--- HelloWorld.WinForms/obj/Debug/net8.0-windows/HelloWorld.WinForms.AssemblyInfo.cs
+++ HelloWorld.WinForms/obj/Debug/net8.0-windows/HelloWorld.WinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld.WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf0f7b74fee3b56b95f211e7187131965f1a50d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02bb4b5a46cfa6b8d0260a781040ee729db1164")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld.WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld.WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
