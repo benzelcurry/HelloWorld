e4a32ce89de2dcc047fd52d31bdbe234407c3f0e
Benzel <benawhiting@gmail.com>
2024-03-07 15:26:17 -0800

    Add class to integrate app with database

---
+++ HelloWorld.Business/MovieService.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld.Business
+{
+    public class MovieService
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;" +
+            "Initial Catalog=MovieLibrary;" +
+            "Integrated Security=True;" +
+            "Connect Timeout=30;" +
+            "Encrypt=False;" +
+            "Trust Server Certificate=False;" +
+            "Application Intent=ReadWrite;" +
+            "Multi Subnet Failover=False";
+    }
+}
--- HelloWorld.Business/obj/Debug/net8.0/HelloWorld.Business.AssemblyInfo.cs
+++ HelloWorld.Business/obj/Debug/net8.0/HelloWorld.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6becd1e1a47df14f7e701efc5018a7d97b91bb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acbef80b578f6996377b6dd15df652f67d7df235")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
