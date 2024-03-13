b532adab6c0277a53c2d1eeb13bb32181dada8f3
Benzel <benawhiting@gmail.com>
2024-03-13 15:40:11 -0700

    Add method for updating movies

--- HelloWorld.Business/MovieService.cs
+++ HelloWorld.Business/MovieService.cs
@@ -81,5 +81,25 @@ namespace HelloWorld.Business
                 command.ExecuteNonQuery();
             }
         }
+
+        public void Update(Movie movie)
+        {
+            if (movie.Id <= 0)
+                throw new Exception("Id can't be zero or less.");
+
+            string query = "UPDATE MOVIES SET Title=@Title, Plot=@Plot, ReleaseDate=@ReleaseDate, Seen=@Seen";
+
+            using (SqlConnection connection = new(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new(query, connection);
+                command.Parameters.Add("@Title", System.Data.SqlDbType.VarChar).Value = movie.Title;
+                command.Parameters.Add("@Plot", System.Data.SqlDbType.VarChar).Value = movie.Plot;
+                command.Parameters.Add("@ReleaseDate", System.Data.SqlDbType.DateTime2).Value = movie.ReleaseDate;
+                command.Parameters.Add("@Seen", System.Data.SqlDbType.Bit).Value = movie.Seen;
+                command.Parameters.Add("@Id", System.Data.SqlDbType.Int).Value = movie.Id;
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
