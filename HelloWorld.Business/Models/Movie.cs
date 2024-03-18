8857e8449728304e2e436316b05fc2158c8e59cb
Benzel <benawhiting@gmail.com>
2024-03-18 14:48:37 -0700

    Set max length for title

--- HelloWorld.Business/Models/Movie.cs
+++ HelloWorld.Business/Models/Movie.cs
@@ -1,8 +1,11 @@
-﻿namespace HelloWorld.Business.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Business.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Title { get; set; }
         public string Plot {  get; set; }
         public DateTime ReleaseDate { get; set; }
--- HelloWorld.WinForms/obj/Debug/net8.0-windows/HelloWorld.WinForms.AssemblyInfo.cs
+++ HelloWorld.WinForms/obj/Debug/net8.0-windows/HelloWorld.WinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld.WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c975419987449cf5dd47ec04ac9f2f34f678fe04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035e8206bb01122024840ba0e79249f865dd65a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld.WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld.WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
