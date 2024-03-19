76b9b95e80ab7cd8bdc1dcfbf95380c5a454a515
Benzel <benawhiting@gmail.com>
2024-03-19 16:12:33 -0700

    Provide a seed for the in-memory db

--- HelloWorld.Business/DataContext.cs
+++ HelloWorld.Business/DataContext.cs
@@ -7,6 +7,28 @@ namespace HelloWorld.Business
     {
         public DbSet<Movie> Movies { get; set; }
 
+        public DataContext()
+        {
+            this.Movies.Add(new Movie()
+            {
+                Id = 3,
+                Plot = "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.",
+                ReleaseDate = new DateTime(1999, 03, 31),
+                Seen = false,
+                Title = "The Matrix"
+            });
+            this.Movies.Add(new Movie()
+            {
+                Id = 4,
+                Plot = "78-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with balloons, inadvertently taking a young stowaway.\r\n\r\n",
+                ReleaseDate = new DateTime(2009, 05, 29),
+                Seen = false,
+                Title = "Up!"
+            });
+
+            this.SaveChanges();
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;" +
--- HelloWorld.Business/obj/Debug/net8.0/HelloWorld.Business.AssemblyInfo.cs
+++ HelloWorld.Business/obj/Debug/net8.0/HelloWorld.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76575d4d20d050ccfa11b8de44795812909ca050")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07968b24cf84d022e11f9099914711df6ac16952")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
